ff2124876725d994cf19dd3244bc543331432087
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-03-03 23:00:00 +0900

    add constants

---
+++ src/constants/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    title: 'まくらA',
+    code: 'pillow-a',
+    price: 10000,
+    image: '/pillow-dummy.png',
+    description: '',
+  },
+  {
+    title: 'まくらB',
+    code: 'pillow-b',
+    price: 9999,
+    image: '/pillow-dummy.png',
+    description: '',
+  },
+  {
+    title: 'まくらC',
+    code: 'pillow-c',
+    price: 12000,
+    image: '/pillow-dummy.png',
+    description: '',
+  },
+  {
+    title: 'まくらD',
+    code: 'pillow-d',
+    price: 8000,
+    image: '/pillow-dummy.png',
+    description: '',
+  },
+] as const
