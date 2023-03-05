7f10add6e37d3da08507c1cc2e4444b10c476a46
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-03-05 17:37:32 +0900

    update top page, products page and products data

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -1,54 +1,60 @@
 export const products = [
   {
-    title: 'まくらA',
-    code: 'pillow-a',
+    title: 'ホット枕',
+    code: 'neckwarmpillow',
     price: 10000,
     image: '/pillow-dummy.png',
     description: '',
     shopifyId: 7110153404621,
     pageMeta: {
-      // metaタグ用のデータ
-      title: '',
-      description: '',
+      title: '【CALQS公式】首と肩がホッとする枕（首を40度で温める枕）枕・寝具の通販サイト | CALQS（カルクス）',
+      description:
+        '就寝前15分間を『心が温まる時間』に。ダブル枕に「首ホッと機能」がつき、首をじんわ～り心地よい温度で温めます。じんわ～り心地よい温度でリラックスできる環境を整え、同時に眠気も誘ってくれ、血行不良が原因の肩こり・首コリにもアプローチしてくれる夢のような枕です。',
       ogpImage: '',
     },
   },
   {
-    title: 'まくらB',
-    code: 'pillow-b',
+    title: 'ダブル枕',
+    code: 'doublepillow',
     price: 9999,
     image: '/pillow-dummy.png',
     description: '',
     shopifyId: 7110153404621,
     pageMeta: {
-      title: '',
-      description: '',
+      title:
+        '【CALQS公式】首と肩のことだけを考えたダブル枕（首を40度で温める枕）枕・寝具の通販サイト | CALQS（カルクス）',
+      description:
+        '「どの枕も合わない」という悩みの為に生まれました。高めで硬めサポートピローと、低めで柔らかめのヘッドピローという2種類の枕組み合わせることで、首と肩の負担が最小限になる寝姿勢を実現します。血行不良が原因の肩こり・首コリにもアプローチ、寝返りがしやすく、いびきや首シワ対策にもなります。',
       ogpImage: '',
     },
   },
   {
-    title: 'まくらC',
-    code: 'pillow-c',
+    title: '美肌枕',
+    code: 'furez',
     price: 12000,
     image: '/pillow-dummy.png',
     description: '',
     shopifyId: 7110153404621,
     pageMeta: {
-      title: '',
-      description: '',
+      title:
+        '【CALQS公式】美肌まくら Furez～フレズ～（枕の摩擦から素肌を守る）　商品情報　枕・寝具の通販サイト | CALQS（カルクス）',
+      description:
+        '睡眠中は『枕と顔の肌は擦れやすい環境』です。頬を守り、高品質シルク枕カバーで顔のたるみ、シワの原因になる摩擦を少しでも抑えていつまでも綺麗な素肌を手に入れてください。枕と顔の"摩擦"や、"寝起きの顔の枕跡（寝跡）"が気になる方におすすめの製品です。',
       ogpImage: '',
     },
   },
   {
-    title: 'まくらD',
-    code: 'pillow-d',
+    title: '腰枕',
+    code: 'lumberwarmpillow',
     price: 8000,
     image: '/pillow-dummy.png',
     description: '',
     shopifyId: 7110153404621,
     pageMeta: {
-      title: '',
-      description: '',
+      title:
+        '【CALQS公式】腰がホッとする枕（腰を温め、伸ばして、休める）　商品情報　枕・寝具の通販サイト | CALQS（カルクス）',
+      description:
+        '「腰痛持ち」の皆さん、原因特定できていますか？「冷えは万病のもと」とされており、様々な体調不良の原因とされています。ホッとする枕は、腰まわりの様々な悩みにアプローチ。温熱効果により血行の滞りを防ぎ、また理想的なS字カーブを描き腰の負担を最大限に軽減します。',
       ogpImage: '',
     },
   },
