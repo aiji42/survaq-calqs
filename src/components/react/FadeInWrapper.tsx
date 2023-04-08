df4f8b0b6e5cd632f346f118e31a8875025e43df
kamifuji.mayu <kamifuji.mayu@a-tm.co.jp>
2023-04-08 16:14:25 +0900

    アニメーション用のコンポーネントを作成

---
+++ src/components/react/FadeInWrapper.tsx
@@ -0,0 +1,16 @@
+import type { FC, ReactNode } from 'react'
+import { useInView } from 'react-intersection-observer'
+import styles from './styles.module.css'
+
+type FadeInWrapper = {
+  children: ReactNode
+}
+
+export const FadeInWrapper: FC<FadeInWrapper> = (props) => {
+  const { ref, inView } = useInView({ triggerOnce: true })
+  return (
+    <div ref={ref} className={inView ? styles.visible : styles.fadeInContent}>
+      {props.children}
+    </div>
+  )
+}
---
+++ src/components/react/SlideAppearWrapper.tsx
@@ -0,0 +1,16 @@
+import type { FC, ReactNode } from 'react'
+import { useInView } from 'react-intersection-observer'
+import styles from './styles.module.css'
+
+type SlideAppearWrapper = {
+  children: ReactNode
+}
+
+export const SlideAppearWrapper: FC<SlideAppearWrapper> = (props) => {
+  const { ref, inView } = useInView({ triggerOnce: true })
+  return (
+    <div ref={ref} className={`${styles.appear} ${inView && styles.wrapper}`}>
+      <div className={`${styles.appear} ${inView && styles.inner}`}>{props.children}</div>
+    </div>
+  )
+}
