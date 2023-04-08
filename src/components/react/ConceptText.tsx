72f3c8861711ed0852cdd16e0887823067105580
kamifuji.mayu <kamifuji.mayu@a-tm.co.jp>
2023-04-08 16:14:55 +0900

    TOPページのコンセプトをコンポーネント化

---
+++ src/components/react/ConceptText.tsx
@@ -0,0 +1,24 @@
+import { useInView } from 'react-intersection-observer'
+import styles from './styles.module.css'
+
+export const ConceptText = () => {
+  const { ref, inView } = useInView({ triggerOnce: true })
+  return (
+    <h2 ref={ref} className={`${styles.concept} ${inView && styles.concept_visible}`}>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible}`}>カ</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_02}`}>ラ</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_03}`}>ダ</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_04}`}>を</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_05}`}>軽</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_02}`}>く</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_03}`}>、</span>
+      <br />
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_04}`}>コ</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_05}`}>コ</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_02}`}>ロ</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_03}`}>も</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_04}`}>軽</span>
+      <span className={`${styles.concept_text} ${inView && styles.concept_visible} ${styles.delay_05}`}>く。</span>
+    </h2>
+  )
+}
